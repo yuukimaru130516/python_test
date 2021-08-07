03e0d0cc1ba0aa9b22af6272ea3755d57614d673
yuukimaru <yuuk130516@gmail.com>
2021-08-07 10:13:54 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def test_if_while():
+    num = 0
+
+    while num < 10:
+        if num < 5:
+            print("Under 5");
+        else:
+            print("Over 5")
+
+        num = num + 1
+
+
+test_if_while()
+
